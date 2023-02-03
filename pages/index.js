6fb4354a84c9a2cc652c8842d5e06d23442e0e24
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-02-03 11:34:50 +0530

    configured tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Facebook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build Facebook</h1>
+      <h1 className="text-3xl font-bold underline">Lets build Facebook</h1>
     </div>
   );
 }
