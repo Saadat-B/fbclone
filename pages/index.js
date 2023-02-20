06899397236e6c96aa9c20d6cdfdd28c1e6d4648
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-02-20 13:05:33 +0530

    Add Login functionality

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
         <title>Facebook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* Header  */}
+      <title>Facebook</title>
       <Header />
       <main>
         {/* Sidebar  */}
@@ -18,3 +22,16 @@ export default function Home() {
     </div>
   );
 }
+
+// With the useSession hook you dont need this below
+
+// export async function getServerSideProps(context) {
+//   // Get the user
+//   const session = await getSession(context);
+
+//   return {
+//     props: {
+//       session,
+//     },
+//   };
+// }
